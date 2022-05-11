ab2c0310ff8c19d3a856054b1d845093e64e21a3
LuigiSouza <lpsouza@inf.ufsm.br>
2022-05-11 14:20:12 -0300

    tictactoe example

--- src/Sequential/DateClient.java
+++ src/Sequential/DateClient.java
@@ -14,7 +14,7 @@ public class DateClient {
             System.err.println("Pass the server IP as the sole command line argument");
             return;
         }
-        var socket = new Socket(args[0], DateServer.PORT);
+        var socket = new Socket(args[0], 59090);
         var in = new Scanner(socket.getInputStream());
         System.out.println("Server response: " + in.nextLine());
         socket.close();
